3b1a1ef2c45b0e4e1cc52acc48edce4d4daafdf8
deosterror <131679666+deosterror@users.noreply.github.com>
2024-09-20 19:46:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching Lunar Drafts", "Playing Touch Football"];
+const statusMessages = ["🌐 Watching Lunar Drafts", "🔨 Made by Deoptify"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
